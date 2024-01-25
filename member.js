2de00515ea9ecb0c70382c9abf8f3ea4653158cc
Nguyen Suong <quangsuongit@gmail.com>
2024-01-25 23:26:44 +0700

    add skills member

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    return {
+        name: 'skillsMember',
+        restrict: 'E',
+        templateUrl: 'app/views/skills-member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'skillsMember'
+    };
+}
